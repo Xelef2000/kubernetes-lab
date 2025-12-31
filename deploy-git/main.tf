0a63a131405a8fbf720f106ebdb961c0cb350b40
Felix Niederer <felix.niederer@adfinis.com>
2025-12-31 12:36:27 +0100

    feat(git server): bumped server version

--- deploy-git/main.tf
+++ deploy-git/main.tf
@@ -8,7 +8,7 @@ resource "helm_release" "pygitserver" {
   name       = var.git_release_name
   repository = "https://xelef2000.github.io/PyGitServer"
   chart      = "pygitserver"
-  version    = "1.3.7"
+  version    = "1.3.8"
   namespace  = kubernetes_namespace.git_ns.metadata[0].name
 
   values = [
